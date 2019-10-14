b6f2308f3ef8cee6206820e19b5e2ee2112d531e
Will Hodgson <will.hodgson@current-rms.com>
2019-10-14 12:28:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,10 @@ class ApplicationController < Sinatra::Base
 	post "/login" do
 		user = User.find_by(:username => params[:username])
 		if user && user.authenticate(params[:password])
+
+		else
+
+		end
 	end
 
 	get "/success" do
